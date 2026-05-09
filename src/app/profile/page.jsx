ba214bc7500ed1ead20706ef69c0a91b095ea285
tanvir <mstanvirislam05@gmail.com>
2026-05-09 22:47:59 +0600

    feat: implement ProfilePage component to display user name from session

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+'use client'
+import { authClient } from '@/lib/auth.client';
+import { CircleDollar } from '@gravity-ui/icons';
+import { Card } from '@heroui/react';
+import { Link } from 'lucide-react';
 
-const ProfilePage = () => {
+const ProfilePage =  () => {
+    const {data} = authClient.useSession();
+    const user =data?.user;
+    
+
     return (
         <div className='h-[70vh]'>
-            profile
+               <Card className="w-[400px]">
+      <CircleDollar aria-label="Dollar sign icon" className="text-primary size-6" role="img" />
+      <Card.Header>
+        <Card.Title>Become an Acme Creator!</Card.Title>
+        <Card.Description>
+          Visit the Acme Creator Hub to sign up today and start earning credits from your fans and
+          followers.
+        </Card.Description>
+      </Card.Header>
+      <Card.Footer>
+        <Link
+          aria-label="Go to Acme Creator Hub (opens in new tab)"
+          href="https://heroui.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Creator Hub
+          <Link.Icon aria-hidden="true" />
+        </Link>
+      </Card.Footer>
+    </Card>
         </div>
     );
 };
